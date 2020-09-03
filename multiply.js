c052f4043eed79b397e5e1c7993c59d1e47e7bc0
Virginia Claire Olmstead <viol5229@colorado.edu>
2020-09-02 21:04:28 -0700

    simple multiply three numbers

--- multiply.js
+++ multiply.js
@@ -2,6 +2,8 @@ function multiply(){
 	//must multiply by 1 for computer to read it as an int
 	var num1 = document.getElementById('num1').value *1;
 	var num2 = document.getElementById('num2').value *1;
+	var max;
+	var min;
 	var answer = 0;
 	//base case
 	if ((num1 === 0) || (num2 === 0)){
@@ -9,12 +11,35 @@ function multiply(){
 		document.getElementById('product').innerHTML = 0;
 	//if answer != 0
 	}else{
-		for(var i = 0; i < num1; i++){
-			answer += num2;
+		if(num1 > num2){
+			max = num1;
+			min = num2;
+		}else{
+			max = num2;
+			min = num1;
+		}
+		for(var i = 0; i < min; i++){
+			answer += max;
 		}
 	}
 	//write answer to element
 	document.getElementById('product').innerHTML = answer;
+}
+
+function multiply2(){
+	var num1 = document.getElementById('number1').value *1;
+	var num2 = document.getElementById('number2').value *1;
+	var num3 = document.getElementById('number3').value *1;
+	var answer = 0;
+	//base case
+	if ((num1 === 0) || (num2 === 0) || (num3 === 0)){
+		//write answer to element
+		document.getElementById('product2').innerHTML = 0;
+	//if answer != 0
+	}else{
+	}
+	//write answer to element
+	document.getElementById('product2').innerHTML = answer;
 }
 
 
@@ -22,10 +47,19 @@ $(document).ready(function () {
 	$("#multiply").click(function () {
 		multiply();
 	});
+	$("#multiply2").click(function () {
+		multiply2();
+	});
 	$("#clearIt").click(function () {
-        num1.value = 0;
-		num2.value = 0;
+        num1.value = '';
+		num2.value = '';
 		document.getElementById('product').innerHTML = "";
+    });
+    $("#clearIt2").click(function () {
+        number1.value = '';
+		number2.value = '';
+		number3.value = '';
+		document.getElementById('product2').innerHTML = "";
     });
 
 });
